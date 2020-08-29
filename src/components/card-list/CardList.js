3c345ae8d9669b9fb9994066876c3df2604b60fd
ThemeGravity <themegravity@gmail.com>
2020-08-29 18:09:53 +0200

    adding files for github pages

---
+++ src/components/card-list/CardList.js
@@ -0,0 +1,9 @@
+import React, {Component} from "react";
+import './card-list.styles.css';
+import {Card} from "../card/Card";
+
+export const CardList = (props) => (
+  <div className="card-list">
+    {props.monsters.map((monster) => <Card key={monster.id} monster={monster} />)}
+  </div>
+);
---
+++ src/components/search-box/SearchBox.js
@@ -0,0 +1,6 @@
+import React, {Component} from "react";
+import './search-box.styles.css';
+
+export const SearchBox = ({placeholder, handleChange}) => (
+    <input className="search" type="search" onChange={handleChange} placeholder={placeholder} />
+);
